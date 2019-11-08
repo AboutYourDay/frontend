2231505e3fa03598b7ff183fa3eb8f85b7e4de1f
Junyup Hong <junyup.hong@gmail.com>
2019-11-08 18:00:56 +0900

    project init setting

---
+++ src/app/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    </v-app-bar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts" src="./app.ts"/>
+
+<style lang="scss" scoped>
+@import './app.scss';
+</style>
