99a557df8ac30460acc902625c622c6f6cd94be5
hmPark <phm3355@ajou.ac.kr>
2019-11-21 11:31:13 +0900

    update

--- src/views/upload/upload.ts
+++ src/views/upload/upload.ts
@@ -13,8 +13,28 @@ export default class Upload extends Vue {
   private albumBucketName = 'photo-about-your-day';
   private bucketRegion = 'ap-northeast-2';
   private IdentityPoolId = 'ap-northeast-2:f4375cb9-6325-43ab-8596-9a05aab09045';
-
   private file = null;
+
+
+  private ui: {
+    fontSize: number,
+    textAlignHorizontal: 'center' | 'flex-start' | 'flex-end',
+    textAlignVertical: 'center' | 'flex-start' | 'flex-end',
+    fontWeight: number,
+    italic: boolean,
+    underline: boolean,
+    color: string,
+  } = {
+    fontSize: 16,
+    textAlignHorizontal: 'center',
+    textAlignVertical: 'center',
+    fontWeight: 400,
+    italic: false,
+    underline: false,
+    color: '#777777',
+  };
+
+  private image: File | null = null;
 
   private handleFileUpload(event) {
     console.log(event);
@@ -53,26 +73,6 @@ export default class Upload extends Vue {
         }
     );
    }
-
-  private ui: {
-    fontSize: number,
-    textAlignHorizontal: 'center' | 'flex-start' | 'flex-end',
-    textAlignVertical: 'center' | 'flex-start' | 'flex-end',
-    fontWeight: number,
-    italic: boolean,
-    underline: boolean,
-    color: string,
-  } = {
-    fontSize: 16,
-    textAlignHorizontal: 'center',
-    textAlignVertical: 'center',
-    fontWeight: 400,
-    italic: false,
-    underline: false,
-    color: '#777777',
-  };
-
-  private image: File | null = null;
   private alignButton(pos: 'flex-start' | 'center' | 'flex-end') {
     this.ui.textAlignHorizontal = pos;
   }
